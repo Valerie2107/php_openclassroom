51e37053af9c88ef1362d5021d8eda649c2e8fa5
valerie2107 <kinkamyl@hotmail.com>
2023-01-17 12:59:46 +0100

    capture ecrans cours

---
+++ README.md
@@ -0,0 +1,2 @@
+projet d'un site de recettes partagées, fait à partir du cours PHP de Openclassroom pour apprendre mon PHP et arriver à raccocher mes connaissances au cours du CF2M
+
--- open.php
+++ open.php
@@ -7,5 +7,28 @@
     <body>
         <h1>Ma page web</h1>
         <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+
+
+        <p> tests sur les variables et la concaténation 
+        <br>
+            <?php $ageUser=17;
+
+            echo $ageUser;
+            ?>
+        <br>
+        <?php
+            $fullname = "Mathieu Nebra";
+            echo "Bonjour $fullname et bienvenue sur le site !";
+            ?>
+        <br>
+        <?php
+            $fullname = 'Mathieu Nebra';
+            echo 'Bonjour ' . $fullname . ' et bienvenue sur le site !'; // OK
+        ?>
+        </p>
+    <h2>calculs simples en php</h2>
+        <p>
+            
+</p>
     </body>
 </html>
